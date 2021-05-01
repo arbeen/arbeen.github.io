0ea7104f516d09fb3b9937e6cf4485619024950a
arbin <arbinshrestha98@gmail.com>
2021-05-01 08:52:12 -0700

    first commit

---
+++ precache-manifest.ae1a282bb753c232cf375acc265919ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf30b53b419aec8b6bdd5dde06ba720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cae633dd2df7cecca578",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae633dd2df7cecca578",
+    "url": "/static/js/main.8a2ee729.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
